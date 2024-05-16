4f4f6f6b2d5cdee31e53283eff273a1617f4e755
Bogdan Kascelan <bogdan.kascelan@student.um.si>
2024-05-16 14:04:19 +0200

    conditional rendering of dashboard based on user role

--- ast_monitor_web/app/__init__.py
+++ ast_monitor_web/app/__init__.py
@@ -12,7 +12,7 @@ mail = Mail()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:nekipass578@localhost/astmonitor'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/astmonitor'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
     # Email configuration
