3062ff9f32fa0d21859f72dd504fcf30b00c14f1
PajovicVanja <vanja.pajovic@student.um.si>
2024-06-28 17:27:42 +0200

    background image fixed

---
+++ frontend/src/Components/BackgroundImage/BackgroundImage.js
@@ -0,0 +1,9 @@
+// src/Components/BackgroundImage/BackgroundImage.js
+import React from 'react';
+import '../../Styles/BackgroundImage.css';
+
+const BackgroundImage = () => {
+  return <div className="background-image"></div>;
+};
+
+export default BackgroundImage;
